9401d15629bb392721cc364c228ebeddef96f944
2021331072Rupak <128206715+2021331072Rupak@users.noreply.github.com>
2025-12-06 23:28:40 +0600

    Add calorie intake, activity, and sleep tracking fields

--- Backend/Models/UserSchema.js
+++ Backend/Models/UserSchema.js
@@ -61,3 +61,97 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+ calorieIntake: [
+        {
+            item: {
+                type: String,
+                required: true,
+            },
+            date: {
+                type: Date,
+                required: true,
+            },
+            quantity: {
+                type: Number,
+                required: true,
+            },
+            quantitytype: {
+                type: String,
+                required: true,
+            },
+            calorieIntake: {
+                type: Number,
+                required: true,
+            },
+        }
+    ],
+    activityLevel: {
+        type: String,
+        required: true,
+    },
+    sleep: [
+        {
+            date: {
+                type: Date,
+                required: true,
+            },
+            durationInHrs: {
+                type: Number,
+                required: true,
+            },
+        },
+    ],
+    steps: [
+        {
+            date: {
+                type: Date,
+                required: true,
+            },
+            steps: {
+                type: Number,
+                required: true,
+            },
+        },
+    ],
+    workouts: [
+        {
+            date: {
+                type: Date,
+                required: true,
+            },
+            exercise: {
+                type: String,
+                required: true,
+            },
+            durationInMinutes: {
+                type: Number,
+                required: true,
+            },
+        },
+    ],
+    water: [
+        {
+            date: {
+                type: Date,
+                required: true,
+            },
+            amountInMilliliters: {
+                type: Number,
+                required: true,
+            },
+        },
+    ],
+}, { timestamps: true });
+
+userSchema.pre('save', async function (next) {
+    const user = this;
+    
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8);
+    }
+    
+    next();
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
